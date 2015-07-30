f0e574008654b2ae1dbcb2e9a1ee16f76cc20119
Nguyen Thanh Tung <nguyenthanhtung@Nguyens-iMac.local>
2015-07-30 18:43:45 +0700

    Initial Commit

---
+++ BreakOutGame/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  BreakOutGame
+//
+//  Created by Nguyen Thanh Tung on 7/30/15.
+//  Copyright (c) 2015 Nguyen Thanh Tung. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
